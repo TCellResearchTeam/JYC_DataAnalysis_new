7ff4181e3301d97ed908da163e55dde32a3ac255
Yolanda-HT <huitiandiao@gmail.com>
2020-05-12 18:01:44 -0400

    initiate

--- z_codes_hpc_1/2_0_jycATAC_DiffBind.RetrieveCount.sh
+++
@@ -1,7 +0,0 @@
-#PBS -l mem=30gb
-
-module unload R # Unload older version of R
-module load R # Load R/3.5.1, because DiffBind package is installed in R/3.5.1
-
-cd /gpfs/home/hdiao/jycATAC/codes
-Rscript 2_0_jycATAC_DiffBind.RetrieveCount.r # Run the R script
--- z_codes_hpc_2/2_2_gff_sizes.sh
+++
@@ -1,2 +0,0 @@
-awk -F "\t" '{gsub("peak_name=", ""); print $9 "\t" $5-$4}' jycATAC_merged_peaks.gff > jycATAC_merged_peaks.sizes
-sort -k1n jycATAC_merged_peaks.sizes > jycATAC_merged_peaks.sizes.srt
